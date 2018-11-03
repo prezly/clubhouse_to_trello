9a864ee84ece97a01f20aae829079919e5ae67f3
Gijs Nelissen <gijs@prezly.com>
2018-11-03 22:56:42 +0100

    Provide aliases for easier use

--- bin/commander.js
+++ bin/commander.js
@@ -11,21 +11,25 @@ const testconfig = require('../lib/testconfig');
 
 program
     .command('start')
+    .alias('create_board')
     .description('Start by creating board and adding the cards')
     .action(() => { start(); });
 
 program
     .command('finish')
+    .alias('update_points')
     .description('Move estimations from trello board back into clubhouse story estimation points')
     .action(() => { finish(); });
 
 program
     .command('clean')
+    .alias('delete')
     .description('Clean up the board/cards/list')
     .action(() => { clean(); });
 
 program
-    .command('testconfig')
+    .command('test')
+    .alias('testconfig')
     .description('Test your config (connect to trello and clubhouse)')
     .action(() => { testconfig(); });
 
